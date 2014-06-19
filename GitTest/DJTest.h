fbd23e606fea58eefe39bb8ea21b1c407655f4ae
Divakaran Jeyachandran <diva@divakarans-mbp.gateway.2wire.net>
2014-06-19 01:25:13 -0700

    added test class

---
+++ GitTest/DJTest.h
@@ -0,0 +1,13 @@
+//
+//  DJTest.h
+//  GitTest
+//
+//  Created by Divakaran Jeyachandran on 6/19/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DJTest : NSObject
+
+@end
---
+++ GitTest/DJTest.m
@@ -0,0 +1,13 @@
+//
+//  DJTest.m
+//  GitTest
+//
+//  Created by Divakaran Jeyachandran on 6/19/14.
+//
+//
+
+#import "DJTest.h"
+
+@implementation DJTest
+
+@end
